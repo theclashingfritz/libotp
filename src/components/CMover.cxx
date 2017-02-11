8ce2552059941e87ff557027065e79a5a8b1e33e
Prince Frizzy <theclashingfritz@gmail.com>
2017-02-10 20:14:46 -0500

    Hmmm

--- src/components/CMover.cxx
+++ src/components/CMover.cxx
@@ -15,3 +15,11 @@ void CMover::set_fwd_speed(int32_t speed) {
 int32_t CMover::get_fwd_speed() {
     return m_fwdSpeed;
 }
+
+void CMover::set_rot_speed(int32_t speed) {
+    m_rotSpeed = speed;
+}
+
+int32_t CMover::get_rot_speed() {
+    return m_rotSpeed;
+}
