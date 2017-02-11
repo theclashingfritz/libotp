66b7c86964d5fd56819fa55e1f61a09641d21abe
ponyboy837 <ponyboy837@gmail.com>
2017-02-10 20:14:46 -0500

    Hmmm

--- src/components/CMover.h
+++ src/components/CMover.h
@@ -13,7 +13,10 @@ class EXPCL_LIBMOVEMENT CMover
         ~CMover();
         void set_fwd_speed(int32_t speed);
         int32_t get_fwd_speed();
+        void set_rot_speed(int32_t speed);
+        int32_t get_rot_speed();
         
     private:
         int32_t m_fwdSpeed;
+        int32_t m_rotSpeed;
 };
