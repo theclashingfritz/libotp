0b2a0453247b258baed139eb3b0f17b0cce0f8c6
Prince Frizzy <theclashingfritz@gmail.com>
2017-02-12 17:56:18 -0500

    There

--- src/nametag/NametagGlobals.cxx
+++ src/nametag/NametagGlobals.cxx
@@ -4,7 +4,15 @@ TypeHandle NametagGlobals::_type_handle;
 
 
 NametagGlobals::NametagGlobals() {
-
+    CFSpeech = 1;
+    CFThought = 2;
+    CFQuicktalker = 4;
+    CFTimeout = 8;
+    CFPageButton = 16;
+    CFQuitButton = 32;
+    CFReversed = 64;
+    CFSndOpenchat = 128;
+    CFNoQuitButton = 256;
 }
 
 NametagGlobals::~NametagGlobals() {
