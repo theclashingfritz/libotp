640017853d1367d160fc24865df443f81e6042ba
ponyboy837 <ponyboy837@gmail.com>
2017-02-12 17:56:18 -0500

    There

--- src/nametag/NametagGlobals.h
+++ src/nametag/NametagGlobals.h
@@ -15,12 +15,12 @@ class EXPCL_LIBOTP NametagGlobals : public TypedObject
     private:   
         uint8_t CFSpeech;
         uint8_t CFThought;
-        uint8_t CFQuickTalker;
+        uint8_t CFQuicktalker;
         uint8_t CFTimeout;
         uint8_t CFPageButton;
         uint8_t CFQuitButton;
         uint8_t CFReversed;
-        uint8_t CFSandOpenchat;
+        uint8_t CFSndOpenchat;
         uint16_t CFNoQuitButton;
         
     TYPE_HANDLE(NametagGlobals, TypedObject);
