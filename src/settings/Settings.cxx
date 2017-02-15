9e8b7ab291bb7f84fbd982412859771e9e78ebcf
Prince Frizzy <theclashingfritz@gmail.com>
2017-02-15 18:27:44 -0500

    Yus

--- src/settings/Settings.cxx
+++ src/settings/Settings.cxx
@@ -26,8 +26,37 @@ void Settings::set_music_volume(float volume) {
     m_music_volume = volume;
 }
 
+void Settings::set_force_sw_midi(bool mode) {
+    m_force_sw_midi = mode;
+}
+
 void Settings::set_embedded_mode(bool mode) {
     m_embedded_mode = mode;
+}
+
+void Settings::set_chat_log(bool mode) {
+    m_log_chat = mode;
+}
+
+void Settings::set_display_driver(int driver) {
+    m_current_driver = driver;
+}
+
+void Settings::set_resolution(int resolution) {
+    m_resolution = resolution;
+}
+
+void Settings::set_resolution_dimensions(unsigned int xsize, unsigned int ysize) {
+    m_resolution_dimensions[0] = xsize;
+    m_resolution_dimensions[1] = ysize;
+}
+
+int Settings::get_resolution() {
+    return m_resolution;
+}
+
+int Settings::get_windowed_mode() {
+    return 1;
 }
 
 float Settings::get_sfx_volume() {
@@ -42,4 +71,7 @@ bool Settings::get_embedded_mode() {
     return m_embedded_mode;
 }
 
+bool Settings::do_saved_settings_exist() {
+    return 0;
+}
 
--- src/settings/Settings.h
+++ src/settings/Settings.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "util.h"
+#include <string>
 #include <typedObject.h>
 #include <pandabase.h>
 
@@ -15,13 +16,26 @@ class EXPCL_LIBOTP Settings : public TypedObject {
         void write_settings();
         void set_sfx_volume(float volume);
         void set_music_volume(float volume);
+        void set_force_sw_midi(bool mode);
         void set_embedded_mode(bool mode);
+        void set_chat_log(bool mode);
+        void set_display_driver(int driver);
+        void set_resolution(int resolution);
+        void set_resolution_dimensions(unsigned int xsize, unsigned int ysize);
+        int get_resolution();
+        int get_windowed_mode();
         float get_sfx_volume();
         float get_music_volume();
         bool get_embedded_mode(); 
+        bool do_saved_settings_exist();
         
     protected:
+        unsigned int m_resolution_dimensions[2];
+        int m_resolution;
+        int m_current_driver;
+        bool m_force_sw_midi;
         bool m_save_settings_exist;
+        bool m_log_chat;
         bool m_embedded_mode;
         float m_music_volume;
         float m_sfx_volume;
